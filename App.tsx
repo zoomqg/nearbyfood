137f874918d52047071c187cbffeafe36439666f
zoomqg <pavlov.misha@gmail.com>
2022-11-23 09:28:43 +0200

    readme

--- App.tsx
+++ App.tsx
@@ -1,11 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native'; 
+import MapView from 'react-native-maps';
+
+import Button from './components/Button';
+
+const onSearch = () =>{
+  console.log('negr');
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView style={styles.map} />
+      <Button label="Search" onPress={onSearch}/>
     </View>
   );
 }
@@ -16,5 +22,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  map: {
+    width: Dimensions.get('screen').width,
+    height: Dimensions.get('screen').height,
   },
 });
---
+++ components/Button.tsx
@@ -0,0 +1,40 @@
+import { StyleSheet, View, Pressable, Text, Dimensions } from 'react-native';
+
+export default function Button({ label, onPress }: any) {
+    return(
+        <View style={styles.buttonContainer}>
+            <Pressable style={styles.button} onPress={onPress}>
+                <Text style={styles.buttonLabel}>{label}</Text>
+            </Pressable>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    buttonContainer: {
+        width: Dimensions.get('screen').width,
+        paddingLeft: 30,
+        paddingRight: 30,
+        height: 56,
+        alignItems: 'center',
+        justifyContent: 'center',
+        position: 'absolute',
+        flex: 1,
+        bottom: 18
+    },
+    button: {
+        borderRadius: 20,
+        width: '100%',
+        height: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'row',
+        backgroundColor: "#0B79D0"
+      },
+      buttonLabel: {
+        color: '#FFFFFF',
+        fontSize: 16,
+        fontWeight: '700',
+        lineHeight: 19
+      },
+});
