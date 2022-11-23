598e2ef70c52ab392da9e3ad7177a6e3a9f04939
zoomqg <pavlov.misha@gmail.com>
2022-11-24 01:37:57 +0200

    lost my sanity because of 1 button

--- App.tsx
+++ App.tsx
@@ -1,20 +1,58 @@
 import { StyleSheet, Text, View, Dimensions } from 'react-native'; 
+import { useState, useEffect, createRef } from 'react';
 import MapView from 'react-native-maps';
-
+import * as Location from 'expo-location';
 import Button from './components/Button';
 
 const onSearch = () =>{
   console.log('negr');
 }
+const calculateRightHeightForLocationButton = () =>{
+  const screen_height = Dimensions.get('screen').height;
+  return screen_height / 16;
+}
 
 export default function App() {
+  const [location, setLocation] = useState<Location.LocationObject | null>(null);
+  const [errorMsg, setErrorMsg] = useState<string | null>(null);
+  const mapRef = createRef<MapView>(); 
+  
+
+  useEffect(() => {
+    (async () => {
+      
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+    })();
+  }, []);
+  let text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} />
+      <MapView ref={mapRef} style={styles.map} 
+          initialRegion={{
+            latitude: 56.95153,
+            longitude: 24.09986,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+          showsUserLocation={true}
+          showsMyLocationButton={true}
+          mapPadding={{top: calculateRightHeightForLocationButton(), bottom: 0, right: 0, left: 0}}
+      />
       <Button label="Search" onPress={onSearch}/>
     </View>
   );
-}
+};
+
 
 const styles = StyleSheet.create({
   container: {
