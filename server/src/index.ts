bc609a39cf34bb37fa6b83ad557727593bfb827d
zoomqg <pavlov.misha@gmail.com>
2023-05-23 00:14:28 +0300

    moved the entire backend to docker

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,7 @@ import { startStandaloneServer } from '@apollo/server/standalone';
 import typeDefs from './schema.js';
 import resolvers from './resolvers/resolvers.js';
 
+const PORT = parseInt(process.env.SERVER_PORT, 10) || 4000;
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
 const server = new ApolloServer({
@@ -15,7 +16,7 @@ const server = new ApolloServer({
 //  2. installs your ApolloServer instance as middleware
 //  3. prepares your app to handle incoming requests
 const { url } = await startStandaloneServer(server, {
-listen: { port: 4000 },
+listen: { port: PORT },
 });
 
 console.log(`🚀  Server ready at: ${url}`);
